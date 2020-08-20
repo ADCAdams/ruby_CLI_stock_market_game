37d301dc015383ae3b5f9c2f3fc1d4e28c00414c
adcadams <andrewdcadams@gmail.com>
2020-08-20 09:08:25 -0400

    added alpha vantage stock api working request and parse

--- lib/api.rb
+++ lib/api.rb
@@ -12,7 +12,25 @@ class GetPrograms
       response.body
     end
    
-  end
+  end     #ends class
    
   programs = GetPrograms.new.get_programs
-  puts programs
+  #puts programs
+
+
+  url = URI("https://alpha-vantage.p.rapidapi.com/query?symbol=TSLA&function=GLOBAL_QUOTE")
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  
+  request = Net::HTTP::Get.new(url)
+  request["x-rapidapi-host"] = 'alpha-vantage.p.rapidapi.com'
+  request["x-rapidapi-key"] = 'b629a43ff0msh128b21c6bcd2d9cp1d32a1jsn84966b99aeea'
+  
+  response = http.request(request)
+  j = JSON.parse(response.body)
+puts j 
+puts j["Global Quote"]["01. symbol"]
+
+  #puts response.body
